17415a0141f97baef368b057ab44a8000e0134dd
rogerparkinson <roger@senanque.co.nz>
2015-11-16 10:34:50 +1300

    locales and messages

---
+++ madura-objects/src/test/java/nz/co/senanque/sandbox/EmailValidatorTest.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c)2014 Prometheus Consulting
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package nz.co.senanque.sandbox;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Locale;
+
+import nz.co.senanque.validationengine.ValidationException;
+import nz.co.senanque.validationengine.annotations.Email;
+import nz.co.senanque.validationengine.fieldvalidators.EmailValidator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * Short description
+ * 
+ * @author Roger Parkinson
+ * @version $Revision: 1.4 $
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/TestBase-spring.xml"})
+@Email
+public class EmailValidatorTest
+{
+    @Autowired private transient MessageSource m_messageSource;
+
+    @Test
+    public void testValidate()
+    {
+        final Email emailDTO = this.getClass().getAnnotation(Email.class);
+        final PropertyMetadataMock propertyMetadata = new PropertyMetadataMock(m_messageSource);
+        propertyMetadata.setClass(String.class);
+        final EmailValidator emailValidator = new EmailValidator();
+        emailValidator.init(emailDTO, propertyMetadata);
+        
+        emailValidator.validate("aa@aa");
+        boolean exception = false;
+        try
+        {
+            emailValidator.validate("aa");
+        }
+        catch (ValidationException e)
+        {
+            exception =true;
+        }
+        assertTrue(exception);
+        
+    }
+
+	public MessageSource getMessageSource() {
+		return m_messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		m_messageSource = messageSource;
+	}
+
+}
---
+++ madura-objects/src/test/java/nz/co/senanque/sandbox/LocaleTest.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c)2014 Prometheus Consulting
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package nz.co.senanque.sandbox;
+
+import static org.junit.Assert.*;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * Short description
+ * 
+ * @author Roger Parkinson
+ * @version $Revision: 1.4 $
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/TestBase-spring.xml"})
+public class LocaleTest
+{
+    @Autowired private transient MessageSource m_messageSource;
+
+    @Test
+    public void testValidate()
+    {
+    	Locale locale = new Locale("fr");
+        Locale.setDefault(locale);
+    	String t = MessageFormat.format("Échec de l''envoi: label = {0} n''est pas une adresse email valide, a tenté = {1}",new Object[]{"aa","bb"});
+    	assertEquals("Échec de l'envoi: label = aa n'est pas une adresse email valide, a tenté = bb",t);
+    }
+
+	public MessageSource getMessageSource() {
+		return m_messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		m_messageSource = messageSource;
+	}
+
+}
