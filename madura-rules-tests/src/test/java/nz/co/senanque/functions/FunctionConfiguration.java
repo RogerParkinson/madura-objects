2a0e9d54d2c0c666c06aff2faa6b5c3ce65814ec
rogerparkinson <roger@senanque.co.nz>
2016-10-25 12:31:10 +1300

    Added functiontest

---
+++ madura-rules-tests/src/test/java/nz/co/senanque/functions/FunctionConfiguration.java
@@ -0,0 +1,11 @@
+package nz.co.senanque.functions;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+		"nz.co.senanque.functions" })
+public class FunctionConfiguration {
+
+}
